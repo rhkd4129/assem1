57322773697ec185405b0ea3c2b1753b99bb409c
Ayeong Kim <devayeong@gmail.com>
2022-04-12 22:49:03 +0900

    init: 기본 리액트 프로젝트 생성

---
+++ assem/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&display=swap" rel="stylesheet" />
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
