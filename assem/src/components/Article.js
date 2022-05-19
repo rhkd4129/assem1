19ffacf671d65c84b976ca8f8181f35980774ea6
DevAyeong <devayeong@gmail.com>
2022-05-19 13:20:38 +0900

    테이블 퍼블리싱 (나중에 대폭 수정)

--- assem/src/components/Article.js
+++ assem/src/components/Article.js
@@ -1,28 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios';
-import Post from '../../actions/Post';
+import React from 'react'
+import Table from '../routes/Table'
+import TableCell from '../routes/TableCell'
+import TableRow from '../routes/TableRow'
 
-const apiUrl = "http://localhost:8000/notice/api/posts";
-
-export default function Article () {
-  const [postList, setPostList] = useState([]);
-
-  useEffect(() => {
-    Axios.get(apiUrl)
-        .then(response=>{
-            const {data}  = response;
-            console.log("loaded response", response);
-            setPostList(data)
-        })
-        .catch(error => {})
-    console.log("mounted");
-}, []);
-return(
-    <div>
-        <h2>PostList</h2>
-        {postList.map(post  =>(
-            <Post post={post} key={post.id}/>// 
-        ))}
-    </div>
-);
+const Article = () => {
+    return (
+        <div>
+            <Table />
+            <TableCell />
+            <TableRow/>
+        </div>
+    )
 }
+
+export default Article;
---
+++ assem/src/routes/TableRow.js
@@ -0,0 +1,7 @@
+import { StyledTableRow } from './Table.styled';
+
+const TableRow = ({ children }) => {
+  return <StyledTableRow>{children}</StyledTableRow>;
+};
+
+export default TableRow;
