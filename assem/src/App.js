ec4bb7db990af6115aeec34de9e6f9a1e49b46df
DevAyeong <devayeong@gmail.com>
2022-05-22 01:36:54 +0900

    api 연동 (테이블 퍼블리싱 x)

--- assem/src/App.js
+++ assem/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
-import Project from './components/Project';
+import Project from './routes/Project';
+import Article from './routes/Article';
 import Calender from './routes/Calender';
 import Home from './routes/Home';
 import Reference from './routes/Reference';
@@ -10,6 +11,7 @@ function App() {
   return (
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/article' element={<Article />} /> 
         <Route path='/project' element={<Project />} />
         <Route path='/calender' element={<Calender />} />
         <Route path='/reference' element={<Reference />} />
